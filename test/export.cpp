49f612ae81f6b1e163f138b88de121a606d2c203
Jim Eckerlein <jim.eckerlein@icloud.com>
2023-03-08 12:29:33 +0100

    Test custom attributes

--- test/export.cpp
+++ test/export.cpp
@@ -199,3 +199,141 @@ TEST(Export, SingleComponent)
         }
     }
 }
+
+TEST(Export, CustomAttributes)
+{
+    {
+        Topology topo;
+        Node node{&topo, 1};
+
+        std::string codename = "marsupial";
+        std::string foo = "foo";
+        int rackNo = 15;
+        node.attrib["codename"] = reinterpret_cast<void *>(&codename);
+        node.attrib["rack_no"] = reinterpret_cast<void *>(&rackNo);
+        node.attrib["unknown_will_not_be_printed"] = reinterpret_cast<void *>(&foo);
+
+        struct My_core_attributes
+        {
+            double temperature;
+            int frequency;
+        };
+
+        My_core_attributes attrib{.temperature = 38.222, .frequency = 2000000000};
+        Core c1{&node, 1};
+        c1.attrib["my_core_info"] = reinterpret_cast<void *>(&attrib);
+
+        auto print_my_attribs = [](string key, void *value, string *ret_value_str) -> int
+        {
+            if (key == "codename" || key == "info")
+            {
+                *ret_value_str = *(string *)value;
+                return 1;
+            }
+            else if (key == "rack_no")
+            {
+                *ret_value_str = std::to_string(*(int *)value);
+                return 1;
+            }
+
+            return 0;
+        };
+
+        auto print_my_custom_attribs = [](string key, void *value, xmlNode *xml) -> int
+        {
+            if (key != "my_core_info")
+            {
+                return 0;
+            }
+
+            auto custom = reinterpret_cast<My_core_attributes *>(value);
+            auto temperatur = std::to_string(custom->temperature);
+            auto frequency = std::to_string(custom->frequency);
+            xmlNode *attribNode = xmlNewNode(nullptr, BAD_CAST(key.c_str()));
+            xmlNewProp(attribNode, BAD_CAST("temperature"), BAD_CAST(temperatur.c_str()));
+            xmlNewProp(attribNode, BAD_CAST("temp_unit"), BAD_CAST("C"));
+            xmlNewProp(attribNode, BAD_CAST("frequency"), BAD_CAST(frequency.c_str()));
+            xmlNewProp(attribNode, BAD_CAST("freq_unit"), BAD_CAST("Hz"));
+
+            xmlNode *attrib = xmlNewNode(nullptr, BAD_CAST("Attribute"));
+            xmlNewProp(attrib, BAD_CAST("name"), BAD_CAST(key.c_str()));
+            xmlAddChild(xml, attrib);
+            xmlAddChild(attrib, attribNode);
+
+            return 1;
+        };
+
+        std::string output_name = "test_export.xml";
+        exportToXml(&topo, output_name, print_my_attribs, print_my_custom_attribs);
+    }
+
+    {
+        validate("test_export.xml");
+
+        auto doc = raii<xmlDoc>{xmlParseFile("test_export.xml"), xmlFreeDoc};
+        ASSERT_NE(doc, nullptr);
+
+        auto pathContext = raii<xmlXPathContext>{xmlXPathNewContext(doc.get()), xmlXPathFreeContext};
+        ASSERT_NE(pathContext, nullptr);
+
+        xmlNode *node = nullptr;
+        ASSERT_NO_THROW(node = getSingleNodeByPath(BAD_CAST("/sys-sage/components/Topology/Node"), pathContext.get()));
+
+        {
+            auto result = raii<xmlXPathObject>{xmlXPathNodeEval(node, BAD_CAST("string(Attribute[1]/@name)"), pathContext.get()), xmlXPathFreeObject};
+            ASSERT_NE(result, nullptr);
+            ASSERT_EQ("codename"_xsv, result->stringval);
+        }
+
+        {
+            auto result = raii<xmlXPathObject>{xmlXPathNodeEval(node, BAD_CAST("string(Attribute[1]/@value)"), pathContext.get()), xmlXPathFreeObject};
+            ASSERT_NE(result, nullptr);
+            ASSERT_EQ("marsupial"_xsv, result->stringval);
+        }
+
+        {
+            auto result = raii<xmlXPathObject>{xmlXPathNodeEval(node, BAD_CAST("string(Attribute[2]/@name)"), pathContext.get()), xmlXPathFreeObject};
+            ASSERT_NE(result, nullptr);
+            ASSERT_EQ("rack_no"_xsv, result->stringval);
+        }
+
+        {
+            auto result = raii<xmlXPathObject>{xmlXPathNodeEval(node, BAD_CAST("string(Attribute[2]/@value)"), pathContext.get()), xmlXPathFreeObject};
+            ASSERT_NE(result, nullptr);
+            ASSERT_EQ("15"_xsv, result->stringval);
+        }
+
+        xmlNode *core = nullptr;
+        ASSERT_NO_THROW(core = getSingleNodeByPath(BAD_CAST("/sys-sage/components/Topology/Node/Core"), pathContext.get()));
+
+        {
+            auto result = raii<xmlXPathObject>{xmlXPathNodeEval(core, BAD_CAST("string(Attribute/@name)"), pathContext.get()), xmlXPathFreeObject};
+            ASSERT_NE(result, nullptr);
+            ASSERT_EQ("my_core_info"_xsv, result->stringval);
+        }
+
+        {
+            auto result = raii<xmlXPathObject>{xmlXPathNodeEval(core, BAD_CAST("string(Attribute/my_core_info/@temperature)"), pathContext.get()), xmlXPathFreeObject};
+            ASSERT_NE(result, nullptr);
+            ASSERT_EQ("38.222000"_xsv, result->stringval);
+        }
+
+        {
+            auto result = raii<xmlXPathObject>{xmlXPathNodeEval(core, BAD_CAST("string(Attribute/my_core_info/@temp_unit)"), pathContext.get()), xmlXPathFreeObject};
+            ASSERT_NE(result, nullptr);
+            ASSERT_EQ("C"_xsv, result->stringval);
+        }
+
+        {
+            auto result = raii<xmlXPathObject>{xmlXPathNodeEval(core, BAD_CAST("string(Attribute/my_core_info/@frequency)"), pathContext.get()), xmlXPathFreeObject};
+            ASSERT_NE(result, nullptr);
+            ASSERT_EQ("2000000000"_xsv, result->stringval);
+        }
+
+        {
+            auto result = raii<xmlXPathObject>{xmlXPathNodeEval(core, BAD_CAST("string(Attribute/my_core_info/@freq_unit)"), pathContext.get()), xmlXPathFreeObject};
+            ASSERT_NE(result, nullptr);
+            ASSERT_EQ("Hz"_xsv, result->stringval);
+        }
+    }
+}
