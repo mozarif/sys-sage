d7bdd86e887f490526e50ee1efedcf3fe168333c
Durganshu <durganshu.mishra@tum.de>
2024-07-14 21:31:53 +0200

    using qdmi ibm-backend-testing branch

--- cmake/Findqdmi.cmake
+++ cmake/Findqdmi.cmake
@@ -3,7 +3,7 @@ include(FetchContent)
 FetchContent_Declare(
     qdmi
     GIT_REPOSITORY git@github.com:Munich-Quantum-Software-Stack/QDMI.git
-    GIT_TAG testing
+    GIT_TAG ibm-backend-testing
 )
 
 FetchContent_MakeAvailable(qdmi)
