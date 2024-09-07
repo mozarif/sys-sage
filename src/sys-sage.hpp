35b66fd24911e4469cf887f12e8c901606e3bf54
Durganshu <durganshu.mishra@tum.de>
2024-09-07 17:32:32 +0200

    Moved parsers to src

--- src/sys-sage.hpp
+++ src/sys-sage.hpp
@@ -5,9 +5,9 @@
 #include "Component.hpp"
 #include "DataPath.hpp"
 #include "xml_dump.hpp"
-#include "external_interfaces/parsers/hwloc.hpp"
-#include "external_interfaces/parsers/caps-numa-benchmark.hpp"
-#include "external_interfaces/parsers/mt4g.hpp"
-#include "external_interfaces/parsers/cccbench.hpp"
+#include "parsers/hwloc.hpp"
+#include "parsers/caps-numa-benchmark.hpp"
+#include "parsers/mt4g.hpp"
+#include "parsers/cccbench.hpp"
 
 #endif //SYS_SAGE
